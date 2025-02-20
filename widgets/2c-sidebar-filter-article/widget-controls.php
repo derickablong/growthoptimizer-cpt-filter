19890786bf9865a510bc8276be64683d025f5bb6
Derick Ablong <derickablong@gmail.com>
2025-02-20 20:12:49 +0800

    Added widget style

--- widgets/2c-sidebar-filter-article/widget-controls.php
+++ widgets/2c-sidebar-filter-article/widget-controls.php
@@ -167,3 +167,176 @@ $this->add_responsive_control(
 );
 
 $this->end_controls_section();
+
+
+
+
+
+# Sidebar
+$this->start_controls_section(
+    'section_2c_photo_article_sidebar_style',
+    [
+        'label' => __( 'Sidebar', 'elementor' ),
+        'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+    ]
+);	
+
+$this->add_control(
+	'sidebar_bg_color',
+	[
+		'label'     => esc_html__( 'Background', 'elementor' ),
+		'type'      => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .go-sidebar' => 'background-color: {{VALUE}};',
+		],
+	]
+);
+
+
+$this->add_group_control(
+    \Elementor\Group_Control_Typography::get_type(),
+    [
+        'name'     => 'sidebar_title_typography',
+        'label'    => 'Title Typography',
+        'selector' => '{{WRAPPER}} .sidebar-title',
+    ]
+);
+
+$this->add_control(
+	'sidebar_title_color',
+	[
+		'label'     => esc_html__( 'Title Color', 'elementor' ),
+		'type'      => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .sidebar-title' => 'color: {{VALUE}};',
+		],
+	]
+);
+
+$this->add_responsive_control(
+    'sidebar_title_spacing',
+    [
+        'type' => \Elementor\Controls_Manager::DIMENSIONS,
+        'label' => esc_html__( 'Margin', 'elementor' ),
+        'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+        'selectors' => [
+            '{{WRAPPER}} .sidebar-title' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+        ],
+    ]
+);
+
+
+$this->add_group_control(
+    \Elementor\Group_Control_Typography::get_type(),
+    [
+        'name'     => 'category_heading_typography',
+        'label'    => 'Category Heading',
+        'selector' => '{{WRAPPER}} .taxonomy-title h6',
+    ]
+);
+
+$this->add_control(
+	'category_heading_color',
+	[
+		'label'     => esc_html__( 'Category Heading Color', 'elementor' ),
+		'type'      => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .taxonomy-title h6' => 'color: {{VALUE}};',
+		],
+	]
+);
+
+$this->add_responsive_control(
+    'category_heading_spacing',
+    [
+        'type' => \Elementor\Controls_Manager::DIMENSIONS,
+        'label' => esc_html__( 'Margin', 'elementor' ),
+        'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+        'selectors' => [
+            '{{WRAPPER}} .taxonomy-title' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+        ],
+    ]
+);
+
+
+$this->add_group_control(
+    \Elementor\Group_Control_Typography::get_type(),
+    [
+        'name'     => 'checkbox_label_typography',
+        'label'    => 'Label Typography',
+        'selector' => '{{WRAPPER}} .terms .label',
+    ]
+);
+
+$this->add_control(
+	'checkbox_label_color',
+	[
+		'label' => esc_html__( 'Label Color', 'elementor' ),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .terms .label' => 'color: {{VALUE}};',
+		],
+	]
+);
+
+$this->add_control(
+	'checkbox_active_color',
+	[
+		'label' => esc_html__( 'Checkbox Active Background', 'elementor' ),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .terms input:checked + .checkbox' => 'background-color: {{VALUE}};',
+		],
+	]
+);
+
+$this->add_responsive_control(
+    'checkbox_item_spacing',
+    [
+        'type'               => \Elementor\Controls_Manager::NUMBER,
+        'label'              => esc_html__( 'Space Bottom', 'elementor' ),
+        'placeholder'        => esc_html__( '0', 'elementor' ),
+        'frontend_available' => true,
+        'devices'            => [ 'desktop', 'tablet', 'mobile' ],
+        'default'            => 9,
+        'tablet_default'     => [
+            'size' => 9,
+            'unit' => 'px'
+        ],
+        'mobile_default'     => [
+            'size' => 9,
+            'unit' => 'px'
+        ],
+        'selectors'          => [
+            '{{WRAPPER}} .taxonomy-group .terms' => 'gap: {{VALUE}}px;',
+        ],
+    ]
+);
+
+$this->end_controls_section();
+
+
+
+
+
+# Content BG
+$this->start_controls_section(
+    'section_2c_photo_article_content_style',
+    [
+        'label' => __( 'Content', 'elementor' ),
+        'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+    ]
+);	
+
+$this->add_control(
+	'content_bg_color',
+	[
+		'label' => esc_html__( 'Background', 'elementor' ),
+		'type' => \Elementor\Controls_Manager::COLOR,
+		'selectors' => [
+			'{{WRAPPER}} .go-content' => 'background-color: {{VALUE}};',
+		],
+	]
+);
+
+$this->end_controls_section();
